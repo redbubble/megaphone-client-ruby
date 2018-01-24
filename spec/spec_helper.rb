3ddbe8b2017dee6edda035826c88956757bb478c
Toby Corkindale <toby.corkindale@redbubble.com>
2018-01-24 17:17:42 +1100

    Delete megaphone ENV vars from ENV before running tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+# Unit tests get confused if these are already set..
+ENV.delete("MEGAPHONE_FLUENT_PORT")
+ENV.delete("MEGAPHONE_FLUENT_MONITORING_PORT")
 require 'megaphone/client'
