abfd9faf9e9a778ca249f2ae23df71d523529231
Toby Corkindale <toby.corkindale@redbubble.com>
2018-01-24 17:47:13 +1100

    Support buffer_overflow callbacks

--- lib/megaphone/client/fluent_logger.rb
+++ lib/megaphone/client/fluent_logger.rb
@@ -6,12 +6,26 @@ module Megaphone
     class FluentLogger
       extend Forwardable
 
-      def_delegators :@logger, :post, :last_error
+      def_delegators :@logger, :post, :last_error, :flush, :close
 
-      def initialize(host, port)
+      def initialize(host, port, overflow_handler = nil)
+        overflow_handler ||= default_overflow_handler
         @logger = Fluent::Logger::FluentLogger.new('megaphone',
                                                    host: host,
-                                                   port: port)
+                                                   port: port,
+                                                   buffer_overflow_handler: overflow_handler
+                                                 )
+      end
+
+      private
+      # A default overflow handler that just prints a warning message.
+      # Production applications should be passing in their own handlers,
+      # which should be alerting monitoring systems!
+      def default_overflow_handler
+        $stderr.puts("Megaphone::Client::FluentLogger - Production apps MUST override buffer overflow handler!")
+        -> (*) {
+          $stderr.puts("Buffer overflow in Megaphone/fluent logger - messages lost")
+        }
       end
 
     end
--- lib/megaphone/client/logger.rb
+++ lib/megaphone/client/logger.rb
@@ -4,10 +4,11 @@ require 'megaphone/client/fluent_logger'
 module Megaphone
   class Client
     class Logger
-      def self.create(host = nil, port = nil)
+
+      def self.create(host = nil, port = nil, overflow_handler = nil)
         if !port.nil? && !port.empty? &&
            !host.nil? && !host.empty?
-          return FluentLogger.new(host, port)
+          return FluentLogger.new(host, port, overflow_handler)
         end
         FileLogger.new
       end
